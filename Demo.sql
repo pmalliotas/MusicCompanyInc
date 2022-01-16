796bf5ade2774885f9d5f62c79fd93cffb94205d
Panagiotis Malliotas <pmalliotas@gmail.com>
2022-01-16 21:01:51 +0200

    v0.3

--- Demo.sql
+++ Demo.sql
@@ -1,4 +1,5 @@
 -- ARTISTS
+
 -- (Artist Name, Type, Number of Members)
 CALL InsertArtistData ('Panos', 'band', 4);
 
@@ -7,31 +8,27 @@ CALL UpdateArtistData(__, 'Stelios and Friends', 'band', 3);
 
 CALL DeleteArtistData(__);
 
+SELECT * FROM GetAllArtists() as Artists;
+
+
+
 -- ALBUMS
+
 -- (Album Name, Artist Id, Release Date, Genre, Number of Downloads, Number of Sales)
-CALL InsertAlbumData (
-    'A Wild Goose',
-    15,
-    '2021-12-09',
-    'Progressive Metal',
-    5000,
-    500
-);
+CALL InsertAlbumData ('A Wild Goose', 15, '2021-12-09', 'Progressive Metal', 5000, 500);
 
 -- (id, Album Name, Artist Id, Release Date, Genre, Number of Downloads, Number of Sales)
-CALL UpdateAlbumData (
-    __,
-    'A Wild Goose',
-    15,
-    '2021-12-09',
-    'Progressive Metal',
-    10000,
-    500
-);
+CALL UpdateAlbumData (__, 'A Wild Goose', 15, '2021-12-09', 'Progressive Metal', 10000, 500);
 
 CALL DeleteAlbumData(__);
 
+SELECT * FROM GetAllAlbums() as Albums;
+
+
+
+
 -- SONGS
+
 -- (Song Name, Album Id, Artist Id, Producer Id, Song Sales, Song Downloads)
 CALL InsertSongData ('Beards of Sweat', 9, 15, 4, 6000, 10000);
 
@@ -40,7 +37,13 @@ CALL UpdateSongData (__, 'Beards of Sweat', 9, 16, 4, 15000, 20000);
 
 CALL DeleteSongData(__);
 
+SELECT * FROM GetAllSongs() as Songs;
+
+
+
+
 -- PRODUCERS
+
 -- (Producer Name, Age, Sex)
 CALL InsertProducerData ('Petros', 28, 'Male');
 
@@ -49,7 +52,13 @@ CALL UpdateProducerData (__, 'Petros', 29, 'Male');
 
 CALL DeleteProducerData(__);
 
+SELECT * FROM GetAllProducers() as Producers;
+
+
+
+
 -- ROOMS 
+
 -- (Room Name, Size in SQM, Floor)
 CALL InsertRoomData('Studio 4', 150, 2);
 
@@ -58,7 +67,13 @@ CALL UpdateRoomData(__, 'Studio 4', 180, 2);
 
 CALL DeleteRoomData(__);
 
+SELECT * FROM GetAllRooms() as Rooms;
+
+
+
+
 -- RESERVATIONS
+
 -- (Time and Date, Artist Id, Producer Id, Room Id)
 CALL InsertReservationData('2022-02-12 16:00:00', 16, 4, 3);
 
@@ -67,7 +82,13 @@ CALL UpdateReservationData(__, '2022-02-12 17:00:00', 16, 5, 3);
 
 CALL DeleteReservationData(__);
 
+SELECT * FROM GetAllReservations() as Reservations;
+
+
+
+
 -- AWARDS
+
 -- (Award Name, Album Id)
 CALL InsertAwardData('Best Performance', 8);
 
@@ -76,44 +97,10 @@ CALL UpdateAwardData(__, 'Best Performance', 9);
 
 CALL DeleteAwardData(__);
 
+SELECT * FROM GetAllAwards() as Awards;
+
+
+
+
 -- LOG
-SELECT
-    *
-FROM
-    GetLog() as Log;
-
---Get Data
-SELECT
-    *
-FROM
-    GetAllArtists() as Artists;
-
-SELECT
-    *
-FROM
-    GetAllAlbums() as Albums;
-
-SELECT
-    *
-FROM
-    GetAllSongs() as Songs;
-
-SELECT
-    *
-FROM
-    GetAllProducers() as Producers;
-
-SELECT
-    *
-FROM
-    GetAllRooms() as Rooms;
-
-SELECT
-    *
-FROM
-    GetAllReservations() as Reservations;
-
-SELECT
-    *
-FROM
-    GetAllAwards() as Awards;
+SELECT * FROM GetLog() as Log;
